94fd069e2591120a38f347b67fcd4de0fd8e323b
tazeek <tazeek.rakib@gmail.com>
2015-09-08 22:52:05 +0800

    replaced tabs with spaces

--- test.py
+++ test.py
@@ -4,31 +4,31 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-	return "Hello World"
+    return "Hello World"
 
 @app.route('/permanent')
 def change():
-	return redirect("https://mmls.mmu.edu.my/", 302)
+    return redirect("https://mmls.mmu.edu.my/", 302)
 
 @app.route('/change')
 def direct():
-	return redirect("http://flask.pocoo.org/", 303)
+    return redirect("http://flask.pocoo.org/", 303)
 
 @app.route('/bad_access')
 def request():
-	abort(400)
+    abort(400)
 
 @app.route('/authenticate')
 def unauthorized():
-	abort(401)
+    abort(401)
 
 @app.route('/forbidden')
 def forbid():
-	abort(403)
+    abort(403)
 
 @app.route('/crash')
 def server():
-	10/0
+    10/0
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
