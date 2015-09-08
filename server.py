5b227f9d43f489d19385841f6aa6f52c93f4c5d3
tazeek <tazeek.rakib@gmail.com>
2015-09-08 22:57:05 +0800

    created server using notepadpp

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, abort
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+    return "Hello World"
+
+@app.route('/permanent')
+def change():
+    return redirect("https://mmls.mmu.edu.my/", 302)
+
+@app.route('/change')
+def direct():
+    return redirect("http://flask.pocoo.org/", 303)
+
+@app.route('/bad_access')
+def request():
+    abort(400)
+
+@app.route('/authenticate')
+def unauthorized():
+    abort(401)
+
+@app.route('/forbidden')
+def forbid():
+    abort(403)
+
+@app.route('/crash')
+def server():
+    10/0
+
+if __name__ == "__main__":
+    app.run(debug = True)
